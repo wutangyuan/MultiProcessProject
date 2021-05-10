60464f24b0deac5c5b0459f4ffc7426450ca20fe
wutangyuan <wutangyuan1@gmail.com>
2021-05-10 21:35:07 +0800

    add file

---
+++ ProcessWindowCaller/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProcessWindowCaller
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (e.Args.Length>0)
+            {
+                MessageBox.Show(e.Args[0]);
+            }
+        }
+    }
+}
